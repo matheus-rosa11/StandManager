73d637e1c4769f2713db8ea31da118c9e189de9c
matheus-rosa11 <matheus.rosa@sptech.school>
2025-10-15 17:27:08 -0300

    Adiciona InitialMigration correta

--- api/StandManager/Migrations/20251015202522_InitialCreate.Designer.cs
+++ api/StandManager/Migrations/20251015202522_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StandManager.Data;
@@ -11,9 +12,11 @@ using StandManager.Data;
 namespace StandManager.Migrations
 {
     [DbContext(typeof(StandManagerDbContext))]
-    partial class StandManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015202522_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
